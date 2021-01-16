845038ac290faa693b33f1375c65f9f14f36bae7
Ben Tyler <ben@stae.co>
2021-01-15 17:14:01 -0800

    feat: add support for squared conversions (#1)
    * feat: add support for squared conversions
    
    * chore: remove return parens
    
    * fix: remove unnecessary name prop from types
    
    * chore: reformat types var
    
    * chore: remove semis
    
    * chore: remove semis

--- index.js
+++ index.js
@@ -8,16 +8,41 @@ function prettify (value, shouldPrettify) {
   return parts.join('.')
 }
 
+/**
+ * Utility for checking conversion validity
+ * Errors if a length measurement is trying to convert to an area measurement
+ * and vice versa
+ * @param {string} fromCategoryType the incoming category type i.e. length or area
+ * @param {string} toCategoryType the outbound category type i.e. length or area
+ */
+function isValidConversion (fromCategoryType, toCategoryType) {
+  if (fromCategoryType !== toCategoryType) {
+    var errorSnippet =
+      fromCategoryType === 'area'
+        ? 'an area measurement to a length measurement'
+        : 'a length measurement to an area measurement'
+    throw new Error(
+      `Invalid conversion. You are trying to convert ${errorSnippet}.`
+    )
+  }
+}
+
 var modifiers = {
-	mi: 0.000189394,
-	ft: 1,
-	in: 12
+  mi: 0.000189394,
+  mi2: 0.00000003587,
+  ft: 1,
+  ft2: 1,
+  in: 12,
+  in2: 144,
 }
 
 var types = {
-  mi: 5280,
-  ft: 1,
-  in: 0.08333336
+  mi: { value: 5280, category: 'length' },
+  mi2: { value: 27880000, category: 'area' },
+  ft: { value: 1, category: 'length' },
+  ft2: { value: 1, category: 'area' },
+  in: { value: 0.08333336, category: 'length' },
+  in2: { value: 0.00694444, category: 'area' },
 }
 
 function Pretty (value) {
@@ -37,20 +62,46 @@ Pretty.prototype.input = function (type) {
 }
 
 Pretty.prototype.mi = function () {
-  return prettify((this.value * this.type) * modifiers.mi, this.prettify) + 'mi'
+  isValidConversion(this.type.category, 'length')
+  return prettify(this.value * this.type.value * modifiers.mi, this.prettify) + 'mi'
+}
+
+Pretty.prototype.mi2 = function () {
+  isValidConversion(this.type.category, 'area')
+  return prettify(this.value * this.type.value * modifiers.mi2, this.prettify) + 'mi2'
 }
 
 Pretty.prototype.ft = function () {
-  return prettify((this.value * this.type) * modifiers.ft, this.prettify) + 'ft'
+  isValidConversion(this.type.category, 'length')
+  return prettify(this.value * this.type.value * modifiers.ft, this.prettify) + 'ft'
+}
+
+Pretty.prototype.ft2 = function () {
+  isValidConversion(this.type.category, 'area')
+  return prettify(this.value * this.type.value * modifiers.ft2, this.prettify) + 'ft2'
 }
 
 Pretty.prototype.in = function () {
-  return prettify((this.value * this.type) * modifiers.in, this.prettify) + 'in'
+  isValidConversion(this.type.category, 'length')
+  return prettify(this.value * this.type.value * modifiers.in, this.prettify) + 'in'
+}
+
+Pretty.prototype.in2 = function () {
+  isValidConversion(this.type.category, 'area')
+  return prettify(this.value * this.type.value * modifiers.in2, this.prettify) + 'in2'
 }
 
 Pretty.prototype.humanize = function () {
-  var value = this.type * this.value
+  var value = this.type.value * this.value
+  var category = this.type.category
   this.prettify = true
+
+  if (category === 'area') {
+    if (value >= 27880000) return this.mi2()
+    // necessary hack because of in2 to 1 ft2 conversion coming in just under 1
+    if (value >= 0.999) return this.ft2()
+    return this.in2()
+  }
   if (value >= 5280) return this.mi()
   if (value >= 1) return this.ft()
   return this.in()
--- test/humanize.js
+++ test/humanize.js
@@ -5,12 +5,16 @@ test('humanize feet to mile', function (t) {
   t.is(pretty(15000).humanize(), '2.84mi')
 })
 
+test('humanize square feet to mile', function (t) {
+  t.is(pretty(27880000).input('ft2').humanize(), '1mi2')
+})
+
 test('humanize feet to ft', function (t) {
   t.is(pretty(1.5).humanize(), '1.5ft')
 })
 
 test('humanize ft to inch', function (t) {
-  t.is(pretty(.5).humanize(), '6in')
+  t.is(pretty(0.5).humanize(), '6in')
 })
 
 test('input: humanize in', function (t) {
@@ -21,9 +25,18 @@ test('input: humanize in', function (t) {
   t.is(pretty(6).input('in').humanize(), '6in')
 })
 
+test('input: humanize square in', function (t) {
+  t.is(pretty(144).input('in2').humanize(), '1ft2')
+})
+
+test('input: humanize square in', function (t) {
+  t.is(pretty(100).input('in2').humanize(), '100in2')
+})
+
 test('input: humanize decimal in', function (t) {
   t.is(pretty(0.4).input('in').humanize(), '0.4in')
 })
+
 test('input: humanize zero', function (t) {
   t.is(pretty(0).input('in').humanize(), '0in')
 })
